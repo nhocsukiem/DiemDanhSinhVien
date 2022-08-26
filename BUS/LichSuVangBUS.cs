dea73d95841d577a55e3d61b72c8671f5d13fe13
hoang lam <hoanglam91.hvn@gmail.com>
2022-08-27 00:20:23 +0700

    Add project files.

---
+++ BUS/LichSuVangBUS.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DAO;
+using DTO;
+
+namespace BUS
+{
+    public class LichSuVangBUS
+    {
+        private static LichSuVangBUS instance;
+
+        public static LichSuVangBUS Instance
+        {
+            get
+            {
+                if (instance == null)
+                    instance = new LichSuVangBUS();
+                return instance;
+            }
+        }
+        public DataTable LayDS_LichSuVang_PDD(int mapdd)
+        {
+            return LichSuVangDAO.Instance.LayDS_LichSuVang_PDD(mapdd);
+        }
+        public int Insert_LichSuVang(LichSuVang lsv)
+        {
+            try
+            {
+                LichSuVangDAO.Instance.Insert_LichSuVang(lsv);
+                return 1;
+            }
+            catch (Exception)
+            {
+                return -1;
+            }
+        }
+        public int KiemTraKhoaNgoai_SinhVien(SinhVien sv)
+        {
+            
+            return LichSuVangDAO.Instance.KiemTraKhoaNgoai_SinhVien(sv);
+        }
+    }
+}
